36487741f0afa99d8aae7fb736a5d93a740cab19
yuuki-nakagami <nakagami.yuuki@lmi.ne.jp>
2022-12-20 15:01:12 +0900

    refactor: describeで共通部分を囲う

--- test/IntegerClosedRange.spec.ts
+++ test/IntegerClosedRange.spec.ts
@@ -1,13 +1,15 @@
 import { IntegerClosedRange } from "../lib/IntegerClosedRange";
 
 describe("IntegerClosedRange Test", () => {
-    test("整数閉区間オブジェクトは下端点を返す", () => {
+    describe("整数閉区間（IntegerClosedRange）を示す", () => {
         const integerClosedRange = new IntegerClosedRange(1, 2);
-        expect(1).toBe(integerClosedRange.underPoint);
-    });
 
-    test("整数閉区間オブジェクトは上端点を返す", () => {
-        const integerClosedRange = new IntegerClosedRange(1, 2);
-        expect(2).toBe(integerClosedRange.topPoint);
+        test("整数閉区間オブジェクトは下端点を返す", () => {
+            expect(1).toBe(integerClosedRange.underPoint);
+        });
+
+        test("整数閉区間オブジェクトは上端点を返す", () => {
+            expect(2).toBe(integerClosedRange.topPoint);
+        });
     });
 });
