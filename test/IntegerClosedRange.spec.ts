ab2ebcd95aae0c30e42676e5bb67330673909ee3
yuuki-nakagami <nakagami.yuuki@lmi.ne.jp>
2022-12-20 15:07:32 +0900

    refactor: beforeEachにまとめてみる

--- test/IntegerClosedRange.spec.ts
+++ test/IntegerClosedRange.spec.ts
@@ -2,15 +2,17 @@ import { IntegerClosedRange } from "../lib/IntegerClosedRange";
 
 describe("IntegerClosedRange Test", () => {
     describe("整数閉区間を示す", () => {
-        let integerClosedRange;
+        let integerClosedRange: IntegerClosedRange;
+
+        beforeEach(() => {
+            integerClosedRange = new IntegerClosedRange(1, 2);
+        });
 
         test("整数閉区間オブジェクトは下端点を返す", () => {
-            integerClosedRange = new IntegerClosedRange(1, 2);
             expect(1).toBe(integerClosedRange.lowerEndPoint);
         });
 
         test("整数閉区間オブジェクトは上端点を返す", () => {
-            integerClosedRange = new IntegerClosedRange(1, 2);
             expect(2).toBe(integerClosedRange.upperEndPoint);
         });
     });
