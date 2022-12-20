8e8e6677f0402a3626d354b896e11f8e13fbc5e7
yuuki-nakagami <nakagami.yuuki@lmi.ne.jp>
2022-12-20 15:06:35 +0900

    refactor: テストごとにインスタンス生成するように修正

--- test/IntegerClosedRange.spec.ts
+++ test/IntegerClosedRange.spec.ts
@@ -2,13 +2,15 @@ import { IntegerClosedRange } from "../lib/IntegerClosedRange";
 
 describe("IntegerClosedRange Test", () => {
     describe("整数閉区間を示す", () => {
-        const integerClosedRange = new IntegerClosedRange(1, 2);
+        let integerClosedRange;
 
         test("整数閉区間オブジェクトは下端点を返す", () => {
+            integerClosedRange = new IntegerClosedRange(1, 2);
             expect(1).toBe(integerClosedRange.lowerEndPoint);
         });
 
         test("整数閉区間オブジェクトは上端点を返す", () => {
+            integerClosedRange = new IntegerClosedRange(1, 2);
             expect(2).toBe(integerClosedRange.upperEndPoint);
         });
     });
