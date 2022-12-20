f52b49006e1f94ab5fca1bfeaa14ef0e7888abb1
yuuki-nakagami <nakagami.yuuki@lmi.ne.jp>
2022-12-20 16:07:39 +0900

    refactor: 境界値を扱えるようにパラメタライズ化

--- test/IntegerClosedRange.spec.ts
+++ test/IntegerClosedRange.spec.ts
@@ -3,11 +3,18 @@ import { Fixture } from "./Fixture";
 
 describe("IntegerClosedRange Test", () => {
     describe("整数閉区間を示す", () => {
-        test("整数閉区間オブジェクトは下端点を返す", () => {
+        test.each`
+            lower                      | description
+            ${Number.MAX_SAFE_INTEGER} | ${"下端点として整数の最大値である9007199254740991を返す"}
+            ${Number.MIN_SAFE_INTEGER} | ${"下端点として整数の最小値である-9007199254740991を返す"}
+            ${0}                       | ${"下端点として0を返す"}
+            ${1}                       | ${"下端点として境界値である1を返す"}
+            ${-1}                      | ${"下端点として境界値である-1を返す"}
+        `("$description", ({ lower }) => {
             const integerClosedRange = Fixture.generateIntegerClosedRange({
-                lowerEndPoint: 1,
+                lowerEndPoint: lower,
             });
-            expect(1).toBe(integerClosedRange.lowerEndPoint);
+            expect(lower).toBe(integerClosedRange.lowerEndPoint);
         });
 
         test("整数閉区間オブジェクトは上端点を返す", () => {
