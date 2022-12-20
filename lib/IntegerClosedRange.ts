694b237eea338d4377e39374ab5dfdbdf501a5a2
yuuki-nakagami <nakagami.yuuki@lmi.ne.jp>
2022-12-20 14:57:49 +0900

    green: 上端点を返す

--- lib/IntegerClosedRange.ts
+++ lib/IntegerClosedRange.ts
@@ -1,11 +1,17 @@
 export class IntegerClosedRange {
     private readonly _underPoint: number;
+    private readonly _topPoint: number;
 
-    constructor(point: number) {
+    constructor(point: number, top: number) {
         this._underPoint = point;
+        this._topPoint = top;
     }
 
     get underPoint(): number {
         return this._underPoint;
+    }
+
+    get topPoint(): number {
+        return this._topPoint;
     }
 }
--- test/IntegerClosedRange.spec.ts
+++ test/IntegerClosedRange.spec.ts
@@ -2,7 +2,12 @@ import { IntegerClosedRange } from "../lib/IntegerClosedRange";
 
 describe("IntegerClosedRange Test", () => {
     test("整数閉区間オブジェクトは下端点を返す", () => {
-        const integerClosedRange = new IntegerClosedRange(1);
+        const integerClosedRange = new IntegerClosedRange(1, 2);
         expect(1).toBe(integerClosedRange.underPoint);
+    });
+
+    test("整数閉区間オブジェクトは上端点を返す", () => {
+        const integerClosedRange = new IntegerClosedRange(1, 2);
+        expect(2).toBe(integerClosedRange.topPoint);
     });
 });
